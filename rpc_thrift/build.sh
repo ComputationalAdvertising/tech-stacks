d6f88259cda9a28780ea6112cb717757a824bc23
zhouyong <zhouyong@hrbeu.edu.cn>
2018-05-26 10:19:29 +0800

    update

--- rpc_thrift/build.sh
+++ rpc_thrift/build.sh
@@ -6,6 +6,8 @@ set -o errexit
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 PROJECT_DIR=$SCRIPT_DIR
 
+sh $SCRIPT_DIR/compile-thrift.sh
+
 export THRIFT_HOME=$PROJECT_DIR/third_party/deps
 
 if [ ! -d ${PROJECT_DIR}/build ]; then mkdir -p $PROJECT_DIR/build; fi
---
+++ rpc_thrift/compile-thrift.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)
+PROJECT_DIR=$SCRIPT_DIR
+
+thrift_cpp_out=$PROJECT_DIR/learning
+
+thrift="$PROJECT_DIR/third_party/deps/bin/thrift"
+
+function compile_thrift() {
+  target=$1
+  if [ -d $target ]; then
+    for file in `ls $target`
+    do 
+      if [ ${file:0-7} == ".thrift" ]; then
+        $thrift -r --gen cpp -o $thrift_cpp_out $target/$file
+      fi
+    done
+  fi
+}
+
+compile_thrift $SCRIPT_DIR/learning/thrift
